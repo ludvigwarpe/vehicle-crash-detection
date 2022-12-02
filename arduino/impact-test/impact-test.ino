6a8dfe8392e47ef54cd29f511b685a7fc60d516e
Ludvig Warpe <ludvig@warpe.se>
2022-12-02 13:36:58 +0100

    Fix KY-031 sensor test

--- arduino/impact-test/impact-test.ino
+++ arduino/impact-test/impact-test.ino
@@ -1,15 +1,17 @@
-const byte interruptPin = 2;
+const int impactPin = 3; //declares the impact sensor @pin 3
+int value;
 
-// the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), Knock, RISING );
+  pinMode(impactPin, INPUT); //sets the sensor as INPUT
 }
-void Knock() {
-  Serial.println("1");
-  delay(2);
-}
+
 
 void loop() {
-  Serial.println("0");}
+  value = digitalRead(knockPin); //reads status of sensor
+
+  if (value == HIGH) {
+    Serial.print("KY-031: ");
+    Serial.println(value);
+  }
+}
