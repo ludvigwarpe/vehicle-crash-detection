ca4f5f095983b1efb98715235ca85d5d75650613
Ludvig Warpe <ludvig@warpe.se>
2022-12-23 12:18:04 +0100

    implement gps data collection and mqtt publishing

--- arduino/arduino_main/arduino_main.ino
+++ arduino/arduino_main/arduino_main.ino
@@ -4,6 +4,8 @@
 #include <ArduinoMqttClient.h>
 #include "arduino_secrets.h"
 #include "IMU_handler.h"
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
 
 // credentials for wifi
 char ssid[] = SECRET_SSID;
@@ -14,10 +16,19 @@ WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 int port = 1883;
 const char broker[] = "test.mosquitto.org";
-const char topic_location[] = "luwa9626/vehicle-crash-detection/sensors/gps/location";
+const char topic_latitude[] = "luwa9626/vehicle-crash-detection/sensors/gps/latitude";
+const char topic_longitude[] = "luwa9626/vehicle-crash-detection/sensors/gps/longitude";
 const char topic_speed[] = "luwa9626/vehicle-crash-detection/sensors/gps/speed";
 const char topic_impact[] = "luwa9626/vehicle-crash-detection/sensors/impact";
 const char topic_flipped[] = "luwa9626/vehicle-crash-detection/sensors/flipped";
+
+//GPS
+TinyGPS gps;
+SoftwareSerial ss(4, 3);
+bool new_gps_data = false;
+float current_lat;
+float current_long;
+float current_speed;
 
 //temporary mock values
 const char location_temp[] = "59.31626265649651, 18.091590409027457";
@@ -40,6 +51,7 @@ void setup() {
 
 
   Serial.begin(9600);
+  ss.begin(9600);
   Serial.println("Starting program!");
   pinMode(impact_pin, INPUT);
   connect_wifi();
@@ -55,11 +67,8 @@ void loop() {
   if (current_time_millis - previous_time_millis >= SAMPLE_RATE) {
 
     mqttClient.poll();  //keeping mqtt connection alive
-
-    /* PUBLISH DATA TO BROKER WITH LOCATION AND SPEED*/
-    send_message(topic_location, location_temp);
-    send_message(topic_speed, speed_temp);
-
+    scan_gps_data(); //Scans and encodes gps data from software serial
+  
     if (has_accelerometer_collision()) {
       Serial.println("Collision detected");
       send_message(topic_impact, "collision");
@@ -77,6 +86,20 @@ void loop() {
     Serial.println("Impact detected!");
     send_message(topic_impact, "knock");
     //delay(1000);
+  }
+
+  if(new_gps_data){
+    get_location_data();
+    get_speed_data();
+    send_message(topic_latitude, current_lat);
+    send_message(topic_longitude, current_long);
+    send_message(topic_speed, current_speed);
+    /*Serial.print("LAT= ");
+    Serial.print(current_lat);
+    Serial.print(" LONG= ");
+    Serial.print(current_long);
+    Serial.print(" SPD= ");
+    Serial.println(current_speed);*/
   }
 
 }
@@ -127,9 +150,38 @@ void send_message(char topic[], char message[]) {
   //Serial.println();
 }
 
+//publishes gps data
+void send_message(char topic[], float message) {
+  mqttClient.beginMessage(topic);
+  mqttClient.print(message);
+  mqttClient.endMessage();
+}
+
 
 // reads the impact sensor for collision
 bool has_impact_collsion() {
 
   return digitalRead(impact_pin) == LOW;
 }
+
+void scan_gps_data(){
+   while (ss.available())
+    {
+      char c = ss.read();
+
+      if (gps.encode(c))
+        new_gps_data = true;
+    }
+  }
+
+void get_location_data(){
+  unsigned long age;
+  gps.f_get_position(&current_lat, &current_long, &age);
+  current_lat = current_lat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : current_lat;
+  current_long = current_long == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : current_long;
+}
+
+void get_speed_data(){
+  current_speed = gps.f_speed_kmph() == TinyGPS::GPS_INVALID_F_SPEED ? 0.0 : gps.f_speed_kmph();
+}
+
