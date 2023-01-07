a8110105672e5fdceb7201b6b20a182ad94f41ba
August Johnson Palm <august.palm.music@gmail.com>
2023-01-07 17:12:15 +0100

    updated secrets

--- arduino/arduino_main/arduino_secrets.h
+++ arduino/arduino_main/arduino_secrets.h
@@ -1,8 +1,2 @@
-#define SECRET_SSID "Tele2_973609"
-#define SECRET_PASS "uzymzy5e"
-//#define SECRET_SSID "Ludvigs iPhone"
-//#define SECRET_PASS "luddepuddemedsinkudde"
-//#define SECRET_SSID "Galaxy A12F785"
-//#define SECRET_PASS "xuqj2217"
-//#define SECRET_SSID "ASUS_D8_2G"
-//#define SECRET_PASS "Pappirull1988"
+#define SECRET_SSID "wifi" // your wifi-name
+#define SECRET_PASS "password" // your password
